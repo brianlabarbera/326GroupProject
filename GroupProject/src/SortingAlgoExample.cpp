5ae4a3759ec6f3e3d09ed9f1b7abe1bb8a627d01
brianlabarbera <74026911+brianlabarbera@users.noreply.github.com>
2023-05-06 01:50:18 -0400

    Add files via upload

---
+++ GroupProject/src/SortingAlgoExample.cpp
@@ -0,0 +1,13 @@
+#include "SortingAlgoExample.h"
+
+const string SortingAlgoExample::NAME = "AlgorithmName";
+
+void SortingAlgoExample::SortAlgo(vector<int>& a, long long int& n) {
+    // a is passed by reference so you can use . rather than ->
+    // n has to be a long long int to not overflow
+    if (n < 0) {
+        // Execute sort
+    } else {
+        // Execute sort, increment n for each step taken
+    }
+}
