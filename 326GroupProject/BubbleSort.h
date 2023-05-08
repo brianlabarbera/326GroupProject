fe2fc2b29dc71c6dee31117744cfa062f53930ab
brianlabarbera <74026911+brianlabarbera@users.noreply.github.com>
2023-05-08 19:12:42 -0400

    Update BubbleSort.h

--- 326GroupProject/BubbleSort.h
+++ 326GroupProject/BubbleSort.h
@@ -13,6 +13,8 @@ public:
     static string GetAlgoName() { return NAME; };
 private:
     static const string NAME;
+    void BubbleSort::swap(int& a, int& b);
+    void BubbleSort::swapN(int& a, int& b)
 };
 
 #endif
